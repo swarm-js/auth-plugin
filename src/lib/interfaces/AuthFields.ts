1446cb45998a96bf38d59c643e1b655faeb09eaa
Guillaume GAGNAIRE <contact@scrola.net>
2023-06-22 08:53:23 +0200

    Export interface for auth fields in model

---
+++ src/lib/interfaces/AuthFields.ts
@@ -0,0 +1,25 @@
+interface AuthFidoCredentials {
+  id: string
+  deviceName: string
+  challenge: any
+  authChallenge: any
+  publicKey: string
+  prevCounter: number
+}
+
+export interface AuthFields {
+  swarmUserAccess: string[]
+  swarmValidated: boolean
+  swarmValidationCode: string
+  swarmMagicLinkCode: string
+  swarmMagicLinkValidity: number
+  swarmPassword: string
+  swarmFacebookId: string
+  swarmGoogleId: string
+  swarmGoogleAuthenticatorSecret: string
+  swarmGoogleAuthenticatorPending: boolean
+  swarmEthereumWallet: string
+  swarmInvited: boolean
+  swarmInvitationCode: string
+  swarmFido2Credentials: AuthFidoCredentials[]
+}
