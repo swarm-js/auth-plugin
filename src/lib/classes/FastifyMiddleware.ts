a6343d0152562cd02dac158c954a770f997d907d
Guillaume GAGNAIRE <contact@scrola.net>
2023-07-24 13:11:06 +0200

    Handle Basic Auth

--- src/lib/classes/FastifyMiddleware.ts
+++ src/lib/classes/FastifyMiddleware.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { AuthPluginOptions } from '../interfaces/AuthPluginOptions'
+import { Crypt } from './Crypt'
 
 export function fastifyMiddleware (conf: AuthPluginOptions) {
   return async function (req: any) {
@@ -15,6 +16,36 @@ export function fastifyMiddleware (conf: AuthPluginOptions) {
           req.totpNeeded = !!decoded.totpNeeded
           req.validationRequired = !!decoded.validationRequired
           req.userToken = decoded
+        } catch {}
+        break
+      case 'Basic':
+        const [email, password]: string[] = Buffer.from(token, 'base64')
+          .toString()
+          .split(':')
+        try {
+          const user = await conf.model.findOne({
+            [conf.emailField]: email
+          })
+          if (!user) throw new Error('User not found')
+
+          const passwordValid = await Crypt.verify(password, user.swarmPassword)
+          if (!passwordValid) throw new Error('Password not valid')
+
+          let totpNeeded = false
+          if (
+            conf.googleAuthenticator &&
+            !user.swarmGoogleAuthenticatorPending &&
+            user.swarmGoogleAuthenticatorSecret
+          )
+            totpNeeded = true
+
+          await conf.onLogin(user)
+
+          req.user = user
+          req.totpNeeded = totpNeeded
+          ;(req.validationRequired =
+            !user.swarmValidated && conf.validationRequired),
+            (req.userToken = null)
         } catch {}
         break
     }
